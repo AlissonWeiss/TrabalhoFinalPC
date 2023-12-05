443cf42cc9c682762291174300d4d2c42b54eb2b
Alisson Weiss <alissonweiss@id.uff.br>
2023-12-05 15:47:29 -0300

    Criando menus

--- mywindow.py
+++ mywindow.py
@@ -1,5 +1,4 @@
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow, QAction, QDesktopWidget, QMenuBar, QMenu
+from PyQt5.QtWidgets import QMainWindow, QAction, QDesktopWidget, QMenuBar, QMenu, QInputDialog, QMessageBox
 
 from mycanvas import *
 
@@ -15,30 +14,56 @@ class MyWindow(QMainWindow):
         self.setWindowTitle("Modelador - Alisson Weiss - 216.031.083")
         self.canvas = MyCanvas()
         self.setCentralWidget(self.canvas)
-        # # create a model object and pass to canvas
-        # self.model = MyModel()
-        # self.canvas.set_model(self.model)
-        # create a Toolbar
 
+        self.__create_menus()
+
+    def __create_menus(self):
         menu_bar = QMenuBar(self)
-        # Creating menus using a QMenu object
-        file_menu = QMenu("&File", self)
-        menu_bar.addMenu(file_menu)
-        # Creating menus using a title
-        edit_menu = menu_bar.addMenu("&Edit")
-        help_menu = menu_bar.addMenu("&Help")
 
-        menu_bar.addMenu(edit_menu)
-        menu_bar.addMenu(help_menu)
+        tools_menu = QMenu("&Tools", self)
 
+        # Cria os itens dentro dos menus
+        self.reset_action = QAction("&Reset all drawings ", self)
+        self.reset_action.triggered.connect(self.clear_all_drawings_event)
+
+        self.tesselation_mesh = QAction("&View tessellation mesh", self)
+        self.tesselation_mesh.triggered.connect(self.view_tesselation_mesh_event)
+
+        self.export_json_data = QAction("&Export JSON data", self)
+        self.export_json_data.triggered.connect(self.export_json_data_event)
+
+        tools_menu.addAction(self.reset_action)
+        tools_menu.addAction(self.tesselation_mesh)
+        tools_menu.addSeparator()
+        tools_menu.addAction(self.export_json_data)
+
+        menu_bar.addMenu(tools_menu)
         self.setMenuBar(menu_bar)
 
-        tb = self.addToolBar("File")
-        fit = QAction(QIcon("icons/reset_icon.png"), "reset", self)
-        tb.addAction(fit)
-        tb.actionTriggered[QAction].connect(self.tbpressed)
+    def clear_all_drawings_event(self):
+        self.canvas.clear_draws()
 
-    def tbpressed(self, a):
-        if a.text() == "reset":
-            #self.canvas.fit_world_to_viewport()
-            self.canvas.clear_draws()
+    def view_tesselation_mesh_event(self):
+        value, ok = QInputDialog.getInt(self, 'Espaçamento entre pontos',
+                                        'Informe o espaçamento entre os pontos da malha: ')
+        if ok:
+            print(value)
+
+    def export_json_data_event(self):
+        mesh_points_distance, ok = QInputDialog.getInt(self, 'Espaçamento entre pontos',
+                                                       'Informe o espaçamento entre os pontos da malha: ')
+        if ok:
+            if mesh_points_distance <= 0:
+                QMessageBox.about(self, "Valor inválido", "O valor do espaçamento deve ser maior que zero.")
+                return
+        else:
+            return
+
+        file_name, ok = QInputDialog.getText(self, 'Nome do arquivo',
+                                                   'Informe o nome do arquivo a ser salvo: ')
+        if ok:
+            if file_name == "" or file_name is None:
+                QMessageBox.about(self, "Valor inválido", "O nome do arquivo deve possuir pelo menos um caractere.")
+                return
+        else:
+            return
