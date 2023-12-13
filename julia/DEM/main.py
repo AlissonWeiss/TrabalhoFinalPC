82f9bf3c7050e5f0e2dbf972abd8dff42b968dc7
Alisson Weiss <alissonweiss@id.uff.br>
2023-12-12 21:09:49 -0300

    PVI resolvido

--- julia/DEM/main.py
+++ julia/DEM/main.py
@@ -1,6 +1,8 @@
 import json
-import numpy as np
+
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def read_json_file(_file):
     with open(_file, "r") as file:
@@ -19,65 +21,91 @@ def read_json_file(_file):
 
     return number_of_elements, coordinates_x, coordinates_y, connect, forces, restrictions
 
-def output_res(res):
-    dict_res = {"resultado": res}
-    with open("output.json", "w") as f:
-        json.dump(dict_res, f)
+
+def output_response(_result):
+    result = {"resultado": _result.tolist()}
+    with open("output_python.json", "w") as f:
+        json.dump(result, f)
+
+
+def choose_particle_to_follow(forces: np.ndarray, restrictions: np.ndarray, choice: str):
+    if choice == "with_force":
+        for i in range(len(forces)):
+            if forces[i] != 0:
+                return i
+
+    if choice == "fixed":
+        for i in range(len(restrictions) - 1):
+            if restrictions[i] != 0 and restrictions[i + 1] == 1:
+                return i
+
+    return 0, 0
+
 
 def main(_file_name):
-    print(".DEM")
-    # read input file
-    N = 600
-    h = 0.00004
+    N = 600  # Steps
+    h = 0.00004  # Step size
     number_of_elements, coordinates_x, coordinates_y, connections, forces, restrictions = read_json_file(_file_name)
     ndofs = 2 * number_of_elements
+
+    forces = np.reshape(np.transpose(forces), (ndofs, 1))
+    restrictions = np.reshape(np.transpose(restrictions), (ndofs, 1))
+
     raio = 1.0
     mass = 7850.0
     kspr = 210000000000.0
-    # [Your connectivity matrix, F and restrs arrays should be converted to Python numpy arrays here]
 
-    F = np.reshape(np.transpose(forces), (ndofs, 1))
-    restrs = np.reshape(np.transpose(restrictions), (ndofs, 1))
+    u = np.zeros((ndofs, 1), dtype=np.float64)
+    v = np.zeros((ndofs, 1), dtype=np.float64)
+    a = np.zeros((ndofs, 1), dtype=np.float64)
+    res = np.zeros((N, 1), dtype=np.float64)
+    res_y = np.zeros((N, 1), dtype=np.float64)
 
-    u = np.zeros((ndofs, 1))
-    v = np.zeros((ndofs, 1))
-    a = np.zeros((ndofs, 1))
-    res = np.zeros(N)
+    fi = np.zeros((ndofs, 1), dtype=np.float64)
+    a[:] = (forces - fi) / mass
 
-    fi = np.zeros((ndofs, 1))
-    a = (F - fi) / mass
+    particle_to_follow = choose_particle_to_follow(forces=forces, restrictions=restrictions, choice="with_force")
+    # particle_to_follow = choose_particle_to_follow(forces=forces, restrictions=restrictions, choice="fixed")
     for i in range(N):
+        print(f"{i + 1} of {N}")
         v += a * (0.5 * h)
         u += v * h
-        # contact
-        fi.fill(0.0)
-        for j in range(number_of_elements):
-            if restrs[2*j] == 1:
-                u[2*j] = 0.0
-            if restrs[2*j + 1] == 1:
-                u[2*j + 1] = 0.0
-            xj = coordinates_x[j] + u[2*j]
-            yj = coordinates_y[j] + u[2*j + 1]
-            for index in range(connections[j, 0]):
-                k = connections[j, index + 1]
-                xk = coordinates_x[k] + u[2*k]
-                yk = coordinates_y[k] + u[2*k + 1]
-                dX = xj - xk
-                dY = yj - yk
-                di = np.sqrt(dX**2 + dY**2)
-                d2 = (di - 2*raio)
-                dx = d2 * dX / di
-                dy = d2 * dY / di
-                fi[2*j] += kspr * dx
-                fi[2*j + 1] += kspr * dy
-        a = (F - fi) / mass
-        v += a * (0.5 * h)
-        res[i] = u[33]
 
-    output_res(res.tolist())
-    x = range(1, N + 1)
+        fi = [0.0] * len(fi)
+        for j in range(number_of_elements):
+
+            if restrictions[2 * j] == 1:
+                u[2 * j] = 0.0
+            if restrictions[2 * j + 1] == 1:
+                u[2 * j + 1] = 0.0
+            xj = coordinates_x[j] + u[2 * j]
+            yj = coordinates_y[j] + u[2 * j + 1]
+            for index in range(1, len(connections[j])):
+                k = connections[j, index]
+                if k == 0:
+                    continue
+                xk = coordinates_x[k-1] + u[2 * k - 2]
+                yk = coordinates_y[k-1] + u[2 * k - 1]
+                d_x = xj - xk
+                d_y = yj - yk
+                di = np.sqrt(d_x * d_x + d_y * d_y)
+                d2 = (di - 2 * raio)
+                dx = d2 * d_x / di
+                dy = d2 * d_y / di
+                fi[2 * j] += kspr * dx
+                fi[2 * j + 1] += kspr * dy
+        # Cálculo da aceleração
+        a = (forces - fi) / mass
+        # Cálculo da velocidade
+        v += a * (0.5 * h)
+        # plot
+        res[i] = u[particle_to_follow]
+
+    output_response(res.flatten())
+    x = np.arange(1, N + 1)
     plt.plot(x, res)
     plt.show()
 
-# Call the main function with your JSON file
-main("pvi.json")
+
+if __name__ == '__main__':
+    main("pvi2.json")
